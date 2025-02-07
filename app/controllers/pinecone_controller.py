97c3dce42318917cfba0b8e6704a55dde01daa3e
Aman Khan <173542493+ballboyredditor@users.noreply.github.com>
2025-02-07 14:36:38 +0530

    query pinecone index

--- app/controllers/pinecone_controller.py
+++ app/controllers/pinecone_controller.py
@@ -20,3 +20,17 @@ async def create_pinecone_index(index_name: str, user_id: str = Depends(get_curr
     response = await services.create_pinecone_index_service(index_name)
     logger.debug("Response from create pinecone index service")
     return response
+
+@router.delete("/delete-pinecone-index")
+async def delete_pinecone_index(index_name: str, user_id: str = Depends(get_current_user_id)):
+    logger.debug("Inside delete pinecone index controller")
+    response = await services.delete_index_service(index_name)
+    logger.debug("Response from delete pinecone index service")
+    return response
+
+@router.get("/query-pinecone-index")
+async def query_pinecone_index(query: str, user_id: str = Depends(get_current_user_id)):
+    logger.debug("Inside query pinecone index controller")
+    response = await services.query_pinecone_index_service(query)
+    logger.debug("Response from query pinecone index service")
+    return response
--- app/services/pinecone_service.py
+++ app/services/pinecone_service.py
@@ -41,3 +41,48 @@ async def create_pinecone_index_service(index_name: str):
     except Exception as e:
         logger.error(f"Failed to create Pinecone index: {e}")
         return {"status_code": 500, "response": str(e)}
+    
+async def delete_index_service(index_name: str):
+    """
+    Delete a Pinecone index asynchronously.
+
+    Args:
+        index_name (str): The name of the index to be deleted.
+
+    Returns:
+        dict: A dictionary containing the status code and the response.
+        Keys:
+            - status_code (int): HTTP-like status code (200 for success, 500 for error).
+            - response (str): A message indicating the status of the index deletion.
+    """
+    try:
+        pinecone_client = PineConeDBService()
+        response = await pinecone_client.delete_index(index_name)
+        return response
+    except Exception as e:
+        logger.error(f"Failed to delete Pinecone index: {e}")
+        return {"status_code": 500, "response": str(e)}
+    
+
+async def query_pinecone_index_service(query:str):
+    """
+    Query a Pinecone index asynchronously.
+
+    Args:
+        index_name (str): The name of the index to query.
+        query_vector (list): The query vector to search for similar records.
+        top_k (int): The number of similar records to retrieve.
+
+    Returns:
+        dict: A dictionary containing the status code and the response.
+        Keys:
+            - status_code (int): HTTP-like status code (200 for success, 500 for error).
+            - response (list): A list of similar records retrieved from the index.
+    """
+    try:
+        pinecone_client = PineConeDBService()
+        response = await pinecone_client.query_data(query, 3)
+        return response
+    except Exception as e:
+        logger.error(f"Failed to query Pinecone index: {e}")
+        return {"status_code": 500, "response": str(e)}
